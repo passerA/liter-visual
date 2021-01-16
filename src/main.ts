5ea85405e8acd595b8b948487d391df0bb948518
Carey_home <Carey.Chen@github.com>
2021-01-17 00:19:58 +0800

    添加vuex

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import './index.css'
 
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
+app.use(store)
 app.use(router)
 
 app.mount('#app')
